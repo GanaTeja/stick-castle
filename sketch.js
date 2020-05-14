548992a91da7004c667f966ccef8337eed190bd0
GanaTeja <64629231+GanaTeja@users.noreply.github.com>
2020-05-14 19:39:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(800,400);
+  //createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(0);  
+  drawSprites();
+  st1=createSprite(100,400,100,400)
+  st2=createSprite(100,200,50,50)
+  st3=createSprite(320,400,300,300)
+  st4=createSprite(540,400,100,400)
+  st5=createSprite(540,200,50,50)
+  st6=createSprite(320,200)
+  
+}
